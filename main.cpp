22760db3e32cd1c49c42119666ddc2b7c5c57f18
James Sung <sjkskywalker@korea.ac.kr>
2018-10-31 05:01:31 -0700

    Netfilter Block v1.0.0

---
+++ main.cpp
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <linux/types.h>
+#include <linux/netfilter.h>		/* for NF_ACCEPT */
+#include <errno.h>
+
+#include <libnetfilter_queue/libnetfilter_queue.h>
+
+#include <libnet.h>
+#include <string.h>
+
+bool isBlocked = 0;
+char blockedHost[70];
+
+void Usage(void)
+{
+	printf("[-] Wrong usage!\n");
+	printf("[-] syntax: ./netfilter_block <host>\n");
+	printf("[-] sample: ./netfilter_block test.gilgil.net\n");
+	
+	return;
+}
+
+void dump(unsigned char* buf, int size) {
+	int i;
+	for (i = 0; i < size; i++) {
+		if (i % 16 == 0)
+			printf("\n");
+		printf("%02x ", buf[i]);
+	}
+}
+
+void filter(unsigned char *data, int ret)
+{
+	struct libnet_ipv4_hdr *libnet_ipv4_data = (struct libnet_ipv4_hdr *)data;
+
+	char httpMethods[7][8] = {"GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS"};
+	bool isHttp = 0;
+	const char *hostLoc = "Host: ";
+	
+	isBlocked = 0;
+
+	if (libnet_ipv4_data->ip_p  == IPPROTO_TCP)
+	{
+		struct libnet_tcp_hdr *libnet_tcp_data = (libnet_tcp_hdr *)(((uint8_t *)(libnet_ipv4_data)) + (libnet_ipv4_data->ip_hl << 2));
+		
+		for (int i = 0; i < 6; i++)
+		{
+			if (!strncmp(httpMethods[i], (const char *)(data + (libnet_ipv4_data->ip_hl << 2) + (libnet_tcp_data->th_off << 2)), strlen(httpMethods[i]))) // if http packet, set flag
+			{
+				isHttp = 1;
+				break;
+			}
+		}
+
+		if (isHttp)
+		{
+			for (int i = 0; i < ret; i++)
+			{
+				if (!strncmp(hostLoc, (const char *)(data + (libnet_ipv4_data->ip_hl << 2) + (libnet_tcp_data->th_off << 2) + i), strlen(hostLoc))) // if found "Host: " location
+				{
+					if (!strncmp(blockedHost, (const char *)(data + (libnet_ipv4_data->ip_hl << 2) + (libnet_tcp_data->th_off << 2) + i + 6), strlen(blockedHost))) // if host matched blockedHost
+					{
+						printf("\n\n*** You cannot access <%s> ***\n\n", blockedHost);
+						isBlocked = 1;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	return;
+}
+/* returns packet id */
+static u_int32_t print_pkt (struct nfq_data *tb)
+{
+	int id = 0;
+	struct nfqnl_msg_packet_hdr *ph;
+	struct nfqnl_msg_packet_hw *hwph;
+	u_int32_t mark,ifi; 
+	int ret;
+	unsigned char *data;
+	
+	ph = nfq_get_msg_packet_hdr(tb);
+	if (ph) {
+		id = ntohl(ph->packet_id);
+		printf("hw_protocol=0x%04x hook=%u id=%u ",
+			ntohs(ph->hw_protocol), ph->hook, id);
+	}
+	
+	hwph = nfq_get_packet_hw(tb);
+	if (hwph) {
+		int i, hlen = ntohs(hwph->hw_addrlen);
+
+		printf("hw_src_addr=");
+		for (i = 0; i < hlen-1; i++)
+			printf("%02x:", hwph->hw_addr[i]);
+		printf("%02x ", hwph->hw_addr[hlen-1]);
+	}
+
+	mark = nfq_get_nfmark(tb);
+	if (mark)
+		printf("mark=%u ", mark);
+
+	ifi = nfq_get_indev(tb);
+	if (ifi)
+		printf("indev=%u ", ifi);
+
+	ifi = nfq_get_outdev(tb);
+	if (ifi)
+		printf("outdev=%u ", ifi);
+	ifi = nfq_get_physindev(tb);
+	if (ifi)
+		printf("physindev=%u ", ifi);
+
+	ifi = nfq_get_physoutdev(tb);
+	if (ifi)
+		printf("physoutdev=%u ", ifi);
+
+	ret = nfq_get_payload(tb, &data);
+	if (ret >= 0) {
+		printf("payload_len=%d ", ret);
+		//dump(data, ret);
+		filter(data, ret);
+	}
+
+	fputc('\n', stdout);
+
+	return id;
+}
+	
+
+static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
+	      struct nfq_data *nfa, void *data)
+{
+	u_int32_t id = print_pkt(nfa);
+	printf("entering callback\n");
+	
+	if (isBlocked) return nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
+	else return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) { Usage(); }
+
+	struct nfq_handle *h;
+	struct nfq_q_handle *qh;
+	struct nfnl_handle *nh;
+	int fd;
+	int rv;
+	char buf[4096] __attribute__ ((aligned));
+
+	strncpy(blockedHost, argv[1], strlen(argv[1]));
+
+	printf("opening library handle\n");
+	h = nfq_open();
+	if (!h) {
+		fprintf(stderr, "error during nfq_open()\n");
+		exit(1);
+	}
+
+	printf("unbinding existing nf_queue handler for AF_INET (if any)\n");
+	if (nfq_unbind_pf(h, AF_INET) < 0) {
+		fprintf(stderr, "error during nfq_unbind_pf()\n");
+		exit(1);
+	}
+
+	printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
+	if (nfq_bind_pf(h, AF_INET) < 0) {
+		fprintf(stderr, "error during nfq_bind_pf()\n");
+		exit(1);
+	}
+
+	printf("binding this socket to queue '0'\n");
+	qh = nfq_create_queue(h,  0, &cb, NULL);
+	if (!qh) {
+		fprintf(stderr, "error during nfq_create_queue()\n");
+		exit(1);
+	}
+
+	printf("setting copy_packet mode\n");
+	if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
+		fprintf(stderr, "can't set packet_copy mode\n");
+		exit(1);
+	}
+
+	fd = nfq_fd(h);
+
+	for (;;) {
+		if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
+			printf("pkt received\n");
+			nfq_handle_packet(h, buf, rv);
+			continue;
+		}
+		/* if your application is too slow to digest the packets that
+		 * are sent from kernel-space, the socket buffer that we use
+		 * to enqueue packets may fill up returning ENOBUFS. Depending
+		 * on your application, this error may be ignored. nfq_nlmsg_verdict_putPlease, see
+		 * the doxygen documentation of this library on how to improve
+		 * this situation.
+		 */
+		if (rv < 0 && errno == ENOBUFS) {
+			printf("losing packets!\n");
+			continue;
+		}
+		perror("recv failed");
+		break;
+	}
+
+	printf("unbinding from queue 0\n");
+	nfq_destroy_queue(qh);
+
+#ifdef INSANE
+	/* normally, applications SHOULD NOT issue this command, since
+	 * it detaches other programs/sockets from AF_INET, too ! */
+	printf("unbinding from AF_INET\n");
+	nfq_unbind_pf(h, AF_INET);
+#endif
+
+	printf("closing library handle\n");
+	nfq_close(h);
+
+	exit(0);
+}
+
